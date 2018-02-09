bf9a77363b460033bfc458c7f91ead0c88a819aa
Denis Jannot <djannot@gmail.com>
2018-02-09 09:55:04 +0100

    Update private-agent.tf

--- multi-region-aws-vpn/private-agent.tf
+++ multi-region-aws-vpn/private-agent.tf
@@ -77,7 +77,7 @@ resource "aws_instance" "agent_group_1" {
 
 # Create DCOS Mesos Agent Scripts to execute
 module "dcos-mesos-agent" {
-  source = "git@github.com:amitaekbote/terraform-dcos-enterprise//tf_dcos_core?ref=addnode"
+  source = "/amitaekbote/terraform-dcos-enterprise//tf_dcos_core?ref=addnode"
   bootstrap_private_ip = "${aws_instance.bootstrap.private_ip}"
   dcos_install_mode    = "${var.state}"
   dcos_version         = "${var.dcos_version}"
